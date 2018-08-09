c0678613700a0a4f83244d11f6a7f4a9eff65fbd
Jason <jason127dn@gmail.com>
2018-08-09 16:24:40 +0800

    practice matplotlib

---
+++ matplotlib_t/mpt02.py
@@ -0,0 +1,134 @@
+from typing import Any, Union
+
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.axes import Axes
+
+#控座標軸
+x = np.linspace(-3, 3, 50)
+y1 = 2*x + 1
+y2 = x**2
+
+plt.figure()
+plt.subplot(2, 3, 1)
+plt.plot(x, y2)
+plt.plot(x, y1, color='red', linewidth=1.0, linestyle='--')
+
+plt.xlim((-1, 2))
+plt.ylim((-2, 3))
+plt.xlabel('I am x')
+plt.ylabel('I am y')
+
+
+new_ticks = np.linspace(-1, 2, 5)
+print(new_ticks)
+plt.xticks(new_ticks)
+
+plt.yticks([-2, -1.8, -1, 1.22, 3],[r'$really\ bad$', r'$bad$', r'$normal$', r'$good$', r'$really\ good$'])
+
+ax: Union[Axes, Any] = plt.gca()
+ax.spines['right'].set_color('none')
+ax.spines['top'].set_color('none')
+ax.xaxis.set_ticks_position('bottom')
+ax.spines['bottom'].set_position(('data', 0))
+ax.yaxis.set_ticks_position('left')
+ax.spines['left'].set_position(('data',0))
+
+#散布圖
+n = 1024    # data size
+X = np.random.normal(0, 1, n) # 每一个点的X值
+Y = np.random.normal(0, 1, n) # 每一个点的Y值
+T = np.arctan2(Y,X) # for color value
+plt.subplot(2, 3, 2)
+plt.scatter(X, Y, s=75, c=T, alpha=.5)
+
+plt.xlim(-1.5, 1.5)
+plt.xticks(())  # ignore xticks
+plt.ylim(-1.5, 1.5)
+plt.yticks(())  # ignore yticks
+
+
+#柱狀圖
+n = 12
+X = np.arange(n)
+Y1 = (1 - X / float(n)) * np.random.uniform(0.5, 1.0, n)
+Y2 = (1 - X / float(n)) * np.random.uniform(0.5, 1.0, n)
+plt.subplot(2, 3, 3)
+plt.bar(X, +Y1)
+plt.bar(X, -Y2)
+
+plt.xlim(-.5, n)
+plt.xticks(())
+plt.ylim(-1.25, 1.25)
+plt.yticks(())
+plt.bar(X, +Y1, facecolor='#9999ff', edgecolor='white')
+plt.bar(X, -Y2, facecolor='#ff9999', edgecolor='white')
+for x, y in zip(X, Y1):
+    # ha: horizontal alignment
+    # va: vertical alignment
+    plt.text(x + 0.4, y + 0.05, '%.2f' % y, ha='center', va='bottom')
+
+for x, y in zip(X, Y2):
+    # ha: horizontal alignment
+    # va: vertical alignment
+    plt.text(x + 0.4, -y - 0.05, '%.2f' % y, ha='center', va='top')
+
+
+#等高線圖
+def f(x,y):
+    # the height function
+    return (1 - x / 2 + x**5 + y**3) * np.exp(-x**2 -y**2)
+
+n = 256
+x = np.linspace(-3, 3, n)
+y = np.linspace(-3, 3, n)
+X,Y = np.meshgrid(x, y)
+
+# use plt.contourf to filling contours
+# X, Y and value for (X,Y) point
+plt.subplot(2, 3, 4)
+plt.contourf(X, Y, f(X, Y), 8, alpha=.75, cmap=plt.cm.hot)
+# use plt.contourf to filling contours
+# X, Y and value for (X,Y) point
+plt.contourf(X, Y, f(X, Y), 8, alpha=.75, cmap=plt.cm.hot)
+
+# use plt.contourf to filling contours
+# X, Y and value for (X,Y) point
+plt.contourf(X, Y, f(X, Y), 8, alpha=.75, cmap=plt.cm.hot)
+# use plt.contour to add contour lines
+C = plt.contour(X, Y, f(X, Y), 8, colors='black', linewidth=.5)
+plt.clabel(C, inline=True, fontsize=10)
+plt.xticks(())
+plt.yticks(())
+
+# 圖片
+a = np.array([0.313660827978, 0.365348418405, 0.423733120134,
+              0.365348418405, 0.439599930621, 0.525083754405,
+              0.423733120134, 0.525083754405, 0.651536351379]).reshape(3,3)
+
+plt.subplot(2, 3, 5)
+plt.imshow(a, interpolation='nearest', cmap='bone', origin='lower')
+plt.colorbar(shrink=.92)
+
+plt.xticks(())
+plt.yticks(())
+
+
+from mpl_toolkits.mplot3d import Axes3D
+
+
+fig = plt.figure()
+ax = Axes3D(fig)
+
+# X, Y value
+X = np.arange(-4, 4, 0.25)
+Y = np.arange(-4, 4, 0.25)
+X, Y = np.meshgrid(X, Y)    # x-y 平面的网格
+R = np.sqrt(X ** 2 + Y ** 2)
+# height value
+Z = np.sin(R)
+
+ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=plt.get_cmap('rainbow'))
+ax.contourf(X, Y, Z, zdir='z', offset=-2, cmap=plt.get_cmap('rainbow'))
+
+plt.show()
--- numpy_t/numpy01.py
+++ numpy_t/numpy01.py
@@ -3,4 +3,10 @@ np1 = np.array([1, 2, 3])
 np2 = np.array([3, 4, 5])
 
 print(np1 + np2)
+print(np1.ndim, np1.shape, np1.dtype)
+
+np3 = np.array([1, 2, 3, 4, 5, 6])
+np3 = np3.reshape([2, 3])
+
+print(np3.ndim, np3.shape, np3.dtype)
 
