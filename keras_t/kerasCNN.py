c0b1442648e2aa5145fdc19432a31c92edf714c2
Jason <jason127dn@gmail.com>
2018-08-14 00:15:04 +0800

    cnn 99.1

--- keras_t/kerasCNN.py
+++ keras_t/kerasCNN.py
@@ -11,13 +11,11 @@ from keras.models import model_from_json
 
 
 model = Sequential()
-model.add(Conv2D(filters=16,kernel_size=(5,5),padding='same',input_shape=(28,28,1),activation='relu'))
+model.add(Conv2D(filters=30,kernel_size=(5,5),padding='same',input_shape=(28,28,1),activation='relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Conv2D(filters=40,kernel_size=(5,5),padding='same',activation='relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Flatten())
-model.add(Dense(units=300, kernel_initializer='normal', activation='relu'))
-model.add(Dropout(0.25))
 model.add(Dense(units=300, kernel_initializer='normal', activation='relu'))
 model.add(Dropout(0.25))
 model.add(Dense(units=300, kernel_initializer='normal', activation='relu'))
@@ -27,14 +25,14 @@ model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accur
 y_Trainonehot = np_utils.to_categorical(y_train)
 y_Testonehot = np_utils.to_categorical(y_test)
 
-x_train_2D = x_train.reshape(60000, 28*28).astype('float32')
-x_test_2D = x_test.reshape(10000, 28*28).astype('float32')
+x_train_2D = x_train.reshape(60000, 28,28,1).astype('float32')
+x_test_2D = x_test.reshape(10000, 28,28,1).astype('float32')
 
 x_Train_norm = x_train_2D/255
 x_Test_norm = x_test_2D/255
 
 # 進行訓練, 訓練過程會存在 train_history 變數中
-train_history = model.fit(x=x_Train_norm, y=y_Trainonehot, validation_split=0.2, epochs=30, batch_size=800, verbose=2)
+train_history = model.fit(x=x_Train_norm, y=y_Trainonehot, validation_split=0.33, epochs=20, batch_size=400, verbose=2)
 
 # 顯示訓練成果(分數)
 scores = model.evaluate(x_Test_norm, y_Testonehot)
