ff5be08735bb748f66c0d5649f89cb5f9ed11384
Jason <jason127dn@gmail.com>
2018-08-18 21:42:55 +0800

    integrate k04 k05

--- keras_t/k04_autoencoder.py
+++ keras_t/k04_autoencoder.py
@@ -1,4 +1,4 @@
-from keras.layers import Dense, Input
+from keras.layers import Dense, Input,Flatten,Reshape
 from keras.models import Sequential, load_model, Model
 from keras.datasets import mnist
 from keras.utils import np_utils
@@ -7,24 +7,28 @@ import matplotlib.pyplot as plt
 
 (x_train, y_train),(x_test,y_test) = mnist.load_data()
 
-x_train_2D = x_train.reshape(60000, 28*28).astype('float32')
-x_test_2D = x_test.reshape(10000, 28*28).astype('float32')
+x_train_2D = x_train.reshape(60000, 28,28,1).astype('float32')
+x_test_2D = x_test.reshape(10000, 28,28,1).astype('float32')
 x_train_norm=(x_train_2D/255.0)
 x_test_norm=(x_test_2D/255.0)
 
-input_img=Input(shape=(784,),name="input")
-encoder=Dense(units=1000,activation='relu',name="d1")(input_img)
+input_img=Input(shape=(28,28,1),name="input")
+x=Flatten()(input_img)
+encoder=Dense(units=1000,activation='relu',name="d1")(x)
 encoder=Dense(units=200,activation='relu',name="d2")(encoder)
 encoder=Dense(units=50,activation='relu',name="d3")(encoder)
-encoder_output=Dense(units=2,name='e_out')(encoder)
+encoder=Dense(units=2,name='e_out')(encoder)
+ecd=Model(input_img,encoder,name='encoder')
+ecd.summary()
 
-decoder=Dense(units=50,activation='relu',name='d4')(encoder_output)
+decoder_input=Input(shape=(2,),name='decoder_input')
+decoder=Dense(units=50,activation='relu',name='d4')(decoder_input)
 decoder=Dense(units=200,activation='relu',name='d5')(decoder)
 decoder=Dense(units=1000,activation='relu',name='d6')(decoder)
 decoder=Dense(units=784,activation='sigmoid',name='d_out')(decoder)
-
-autoencoder = Model(input=input_img, output=decoder)
-encode = Model(input=input_img, output=encoder_output)
+decoder=Reshape((28,28,1))(decoder)
+dcd=Model(decoder_input,decoder,name='decoder')
+autoencoder = Model(input_img,dcd(ecd(input_img)),name='autoencoder')
 
 autoencoder.compile(loss='mean_squared_error',optimizer='adam')
 
@@ -33,7 +37,7 @@ train_history = autoencoder.fit(x=x_train_norm, y=x_train_norm, validation_split
 autoencoder.save_weights("autoecd.weight")
 autoencoder.save('autoecd.h5')
 
-encoded_imgs = encode.predict(x_test_norm)
+encoded_imgs = ecd.predict(x_test_norm)
 plt.scatter(encoded_imgs[:, 0], encoded_imgs[:, 1], c=y_test)
 plt.colorbar()
 plt.show()
--- keras_t/k06_autoencoder_model_test.py
+++ keras_t/k06_autoencoder_model_test.py
@@ -5,19 +5,20 @@ from keras.utils import np_utils
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
-
+model_name=['models/autoencodercnn.h5','autoecd.h5']
 image_size=28
 (x_train, y_train),(x_test,y_test) = mnist.load_data()
 
-x_train_2D = x_train.reshape(60000, 28*28).astype('float32')
-x_test_2D = x_test.reshape(10000, 28*28).astype('float32')
+x_train_2D = x_train.reshape(60000, 28,28,1).astype('float32')
+x_test_2D = x_test.reshape(10000, 28,28,1).astype('float32')
 x_train_norm=(x_train_2D/255.0)
 x_test_norm=(x_test_2D/255.0)
 
-autoencoder = load_model('autoecd.h5')
+autoencoder = load_model(model_name[0])
 
-#encoder = Model(inputs=autoencoder.input,outputs=autoencoder.get_layer("e_out").output)
-encoder = Model(inputs=autoencoder.layers[0].input,outputs=autoencoder.layers[5].output)
+#encoder = Model(inputs=autoencoder.input,outputs=autoencoder.get_layer("encoder").output)
+#encoder = Model(inputs=autoencoder.layers[0].input,outputs=autoencoder.layers[5].output)
+encoder=autoencoder.get_layer('encoder')
 encoded_imgs=encoder.predict(x_test_norm)
 plt.scatter(encoded_imgs[:, 0], encoded_imgs[:, 1], c=y_test)
 plt.colorbar()
@@ -25,8 +26,7 @@ plt.show()
 
 # Predict the Autoencoder output from corrupted test images
 x_decoded = autoencoder.predict(x_test_norm)
-x_decoded=x_decoded.reshape(10000,28,28)
-x_test_norm=x_test_norm.reshape(10000,28,28)
+
 
 # Display the 1st 8 corrupted and denoised images
 rows, cols = 10, 30
