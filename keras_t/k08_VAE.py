6227b045cc8503209fd36cda4bd75f9319baf367
Jason <jason127dn@gmail.com>
2018-08-20 14:32:17 +0800

    add vae edit readme.md

---
+++ keras_t/k08_VAE.py
@@ -0,0 +1,68 @@
+from keras.layers import Dense, Input,Lambda
+from keras.models import Sequential, load_model, Model
+from keras.losses import binary_crossentropy
+from keras.datasets import mnist
+from keras.utils import np_utils
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+from keras import backend as K
+
+(x_train, y_train),(x_test,y_test) = mnist.load_data()
+
+x_train_2D = x_train.reshape(60000, 28,28,1).astype('float32')
+x_test_2D = x_test.reshape(10000, 28,28,1).astype('float32')
+x_train_norm=(x_train_2D/255.0)
+x_test_norm=(x_test_2D/255.0)
+
+batch_size = 100
+original_dim = 784
+latent_dim = 2
+intermediate_dim = 256
+nb_epoch = 50
+epsilon_std = 1.0
+
+x = Input(batch_shape=(batch_size, original_dim))#1
+h = Dense(intermediate_dim, activation='relu')(x)#2
+z_mean = Dense(latent_dim)(h)
+z_log_var = Dense(latent_dim)(h)
+
+def sampling(args):
+    z_mean, z_log_var = args
+    epsilon = K.random_normal(shape=(batch_size, latent_dim), mean=0.,stddev=epsilon_std)
+    return z_mean + K.exp(z_log_var / 2) * epsilon
+
+
+z = Lambda(sampling, output_shape=(latent_dim,))([z_mean, z_log_var])
+decoder_h = Dense(intermediate_dim, activation='relu')
+decoder_mean = Dense(original_dim, activation='sigmoid')
+h_decoded = decoder_h(z)
+x_decoded_mean = decoder_mean(h_decoded)
+
+def vae_loss(x, x_decoded_mean):
+    xent_loss = original_dim * binary_crossentropy(x, x_decoded_mean)
+    kl_loss = - 0.5 * K.sum(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)
+    return xent_loss + kl_loss
+
+vae = Model(x, x_decoded_mean)
+vae.compile(optimizer='rmsprop', loss=vae_loss)
+
+# train the VAE on MNIST digits
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+
+vae.fit(x_train, x_train,
+        shuffle=True,
+        nb_epoch=nb_epoch,
+        batch_size=batch_size,
+validation_data=(x_test, x_test))
+
+decoder_input = Input(shape=(latent_dim,))
+_h_decoded = decoder_h(decoder_input)
+_x_decoded_mean = decoder_mean(_h_decoded)
+generator = Model(decoder_input, _x_decoded_mean)
+
--- rm.md
+++ rm.md
@@ -1,2 +1,73 @@
-test pull and push
+#machine learning note
 
+learning resource
+
+* [Hung-yi lee](http://speech.ee.ntu.edu.tw/~tlkagk/courses.html),[youtube channel](https://www.youtube.com/channel/UC2ggjtuuWvxrHHHiaDH1dlQ)
+* [morvanzhou](https://morvanzhou.github.io/)
+* [keras](https://keras.io/zh/)
+
+## 0.Start with Python coding
+* tutorial
+
+[basic python skill](https://www.youtube.com/playlist?list=PL6gx4Cwl9DGAcbMi1sH6oAMk4JHw91mC_)
+
+* IDE 
+
+[pycharm](https://www.jetbrains.com/pycharm/)
+
+ 
+## 1.[Introduction to machine learning](https://www.youtube.com/watch?v=CXgbekl66jc)
+
+#### 1.1 what is machine learning 
+finding a function 
+
+example:
+* speech recognition    sound -> text
+* image recognition     image -> object
+
+what can we do 
+
+* prediction
+* classification
+* ...
+
+####1.2 how did mach
+problem we will solute
+1. prodiction
+2. classification
+    1) binary
+    2) multi-class
+3.ine learning work
+
+three step:
+
+* Define a set of function  
+    seting your model(linear,nonlinear,deep)
+
+* Goodness of function f  
+    define loss function
+
+* Pick the best one  
+    make loss function as small as you can
+    
+
+##2 regression
+output a scalar  
+
+####2.1
+three step:
+1. Model(linear)  
+y=b+wx
+
+2. loss function  
+main-squire-error  
+L(f)=L(w,b)=sum(yh-(wx+b))
+
+3. gradient descent
+
+problem:  
+overfitting underfitting(how to find best model)
+
+####2.2
+
+    
