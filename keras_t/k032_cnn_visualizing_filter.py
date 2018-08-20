cadd3274a53e50384a6d717c833d9715376e3067
Jason <jason127dn@gmail.com>
2018-08-20 22:22:37 +0800

    cnn training weight as models/cnn.h5

--- keras_t/k032_cnn_visualizing_filter.py
+++ keras_t/k032_cnn_visualizing_filter.py
@@ -2,16 +2,19 @@ from keras.models import load_model,Model
 from keras.layers import Dense,Conv2D,Reshape,Input,Activation
 import numpy as np
 
-model_path='my_model.h5'
+model_path='models/cnn.h5'
+
+
 cnn_model=load_model(model_path)
 input_data=np.ones((250000,1))
-f_layer=Input(shape=(1,))
-i_layer=Dense(units=28*28)(f_layer)
-i_layer=Activation('sigmoid',name='sa')(i_layer)
-i_layer=Reshape((28,28,1))(i_layer)
-head=Model(f_layer,i_layer,name="head")
+
+i_layer=Input(shape=(1,))
+x=Dense(units=28*28)(i_layer)
+x=Activation('sigmoid',name='sa')(x)
+x=Reshape((28,28,1))(x)
+head=Model(i_layer,x,name="head")
 head.summary()
-cnn_model.summary()
-cnn_model.input=head.output
-visualizer=Model(f_layer,cnn_model,name='first_visualizer')
+all_m=Model(i_layer,cnn_model(head(i_layer)))
+all_m.summary()
+
 print("1")
--- keras_t/k03_CNN.py
+++ keras_t/k03_CNN.py
@@ -1,7 +1,8 @@
 import numpy as np
 from keras.models import Sequential
 from keras.datasets import mnist
-from keras.layers import Dense, Dropout,Activation, Flatten , Conv2D , MaxPooling2D, Dropout
+from keras.layers import Input, Dense, Dropout,Activation,Reshape, Flatten , Conv2D , MaxPooling2D, Dropout
+from keras.models import Model
 from keras.utils import np_utils
 from matplotlib import pyplot as plt
 from keras.models import model_from_json
@@ -9,7 +10,8 @@ from keras.models import model_from_json
 
 (x_train, y_train),(x_test, y_test) = mnist.load_data()
 
-
+'''
+# model is sequential
 model = Sequential()
 model.add(Conv2D(filters=30,kernel_size=(5,5),padding='same',input_shape=(28,28,1),activation='relu',name='c1'))
 model.add(MaxPooling2D(pool_size=(2,2),name='p1'))
@@ -20,8 +22,33 @@ model.add(Dense(units=300, kernel_initializer='normal', activation='relu'))
 model.add(Dropout(0.25))
 model.add(Dense(units=300, kernel_initializer='normal', activation='relu'))
 model.add(Dense(units=10, kernel_initializer='normal',activation='softmax'))
+'''
+#
+input_layer=Input((28,28,1),name='inlayer')
+x=input_layer
+x=Conv2D(filters=30,kernel_size=(5,5),padding='same',input_shape=(28,28,1),activation='relu',name='c1')(x)
+x=MaxPooling2D(pool_size=(2,2),name='p1')(x)
+x=Conv2D(filters=40,kernel_size=(5,5),padding='same',activation='relu',name='c2')(x)
+x=MaxPooling2D(pool_size=(2,2),name='p2')(x)
+x=Flatten()(x)
+x=Dense(units=300, kernel_initializer='normal', activation='relu')(x)
+x=Dropout(0.25)(x)
+x=Dense(units=300, kernel_initializer='normal', activation='relu')(x)
+x=Dense(units=10, kernel_initializer='normal',activation='softmax')(x)
+model=Model(input_layer,x)
+model.summary()
+#
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
+'''
+i_layer=Input(shape=(1,))
+x=Dense(units=28*28)(i_layer)
+x=Activation('sigmoid',name='sa')(x)
+x=Reshape((28,28,1))(x)
+head=Model(i_layer,x,name="head")
+head.summary()
+all_m=Model(i_layer,model(head(i_layer)))
+all_m.summary()
+'''
 y_Trainonehot = np_utils.to_categorical(y_train)
 y_Testonehot = np_utils.to_categorical(y_test)
 
@@ -38,6 +65,10 @@ train_history = model.fit(x=x_Train_norm, y=y_Trainonehot, validation_split=0.33
 scores = model.evaluate(x_Test_norm, y_Testonehot)
 print()
 print("\t[Info] Accuracy of testing data = {:2.1f}%".format(scores[1]*100.0))
+
+
+model.save_weights("models/cnn.weight")
+model.save('models/cnn.h5')
 
 # 預測(prediction)
 X = x_Test_norm[0:10,:]
@@ -57,7 +88,3 @@ plt.xlabel('Epoch')
 plt.legend(['loss', 'val_loss'], loc='upper left')
 plt.show()
 
-
-model.save_weights("model/cnn.weight")
-
-model.save('my_model.h5')
