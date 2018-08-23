2b27582a98d06f1bbdd72ea557c66b3bafc38492
Jason <jason127dn@gmail.com>
2018-08-23 17:00:32 +0800

    acc 75 val acc 52

--- hw3/face_recognition.py
+++ hw3/face_recognition.py
@@ -1,11 +1,11 @@
 from keras import backend as K
 from keras.models import load_model,Model
-from keras.layers import Dropout, Flatten,Dense,Conv2D,Reshape,Input,Activation,MaxPooling2D
+from keras.layers import BatchNormalization, Dropout, Flatten,Dense,Conv2D,Reshape,Input,Activation,MaxPooling2D
 from keras.utils import np_utils
 import numpy as np
 import matplotlib.pyplot as plt
 import csv
-
+from keras import regularizers
 
 label = []
 feature = []
@@ -43,22 +43,28 @@ y_test = labelc[20000:]
 
 input_layer=Input((48,48,1),name='intput_layer')
 x=Conv2D(filters=20,kernel_size=(5,5),name='conv1',padding='same',activation='relu')(input_layer)
-#x=Conv2D(filters=30,kernel_size=(5,5),name='conv2',padding='same',activation='relu')(x)
-x=MaxPooling2D(pool_size=(3,3),name='p1')(x)
-x=Conv2D(filters=50,kernel_size=(3,3),name='conv3',padding='same',activation='relu')(x)
-#x=Conv2D(filters=30,kernel_size=(3,3),name='conv4',padding='same',activation='relu')(x)
-x=MaxPooling2D(pool_size=(3,3),name='p2')(x)
+x=MaxPooling2D(pool_size=(2,2),name='p1')(x)
+x=Conv2D(filters=60,kernel_size=(5,5),name='conv3',padding='same',activation='relu')(x)
+x=MaxPooling2D(pool_size=(2,2),name='p3')(x)
 x=Flatten(name='f1')(x)
-x=Dense(units=1000,kernel_initializer='normal',activation='relu')(x)
-x=Dropout(0.5)(x)
 x=Dense(units=300,kernel_initializer='normal',activation='relu')(x)
-x=Dropout(0.5)(x)
+x=Dropout(0.25)(x)
+x=Dense(units=100,kernel_initializer='normal',activation='relu')(x)
+x=Dropout(0.25)(x)
 x=Dense(units=7,kernel_initializer='normal',activation='softmax')(x)
 
 model=Model(input_layer,x)
 model.summary()
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-train_history = model.fit(x=x_train, y=y_train, validation_split=0.33, epochs=20, batch_size=400)
+train_history = model.fit(x=x_train, y=y_train, validation_split=0.2, epochs=20, batch_size=400)
 
 
+
+plt.plot(train_history.history['loss'])
+plt.plot(train_history.history['val_loss'])
+plt.title('Train History')
+plt.ylabel('loss')
+plt.xlabel('Epoch')
+plt.legend(['loss', 'val_loss'], loc='upper left')
+plt.show()
